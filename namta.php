3612586771b555be394d22bd1ecb6811ca0d02e7
aminul50 <amunuli50@gmail.com>
2022-03-19 22:37:01 +0800

    2,3,4,5 er namta

---
+++ namta.php
@@ -0,0 +1,38 @@
+<?php
+
+
+for ($i =1; $i <=10;$i++ ){
+    $val = 2 * $i;
+
+echo "2*$i = $val  <br>";
+}
+
+echo "<hr>";
+
+for ($i =1; $i <=10;$i++ ){
+    $val = 3 * $i;
+
+echo "3*$i = $val  <br>";
+}
+
+echo "<hr>";
+
+for ($i =1; $i <=10;$i++ ){
+    $val = 4 * $i;
+
+echo "4*$i = $val  <br>";
+}
+
+
+echo "<hr>";
+
+for ($i =1; $i <=10;$i++ ){
+    $val = 5 * $i;
+
+echo "5*$i = $val  <br>";
+}
+
+
+
+
+?>
