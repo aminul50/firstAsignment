fb78a2896c0fa22be4b3ff8e7a5dbe203f730b4f
aminul50 <amunuli50@gmail.com>
2022-03-20 13:17:04 +0800

    Age calculation

---
+++ age.php
@@ -0,0 +1,8 @@
+<?php
+
+include_once "./ageCalculation.php";
+
+
+ageCal('Aminul',1994);
+
+?>
---
+++ ageCalculation.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Age calculation function
+ */
+
+
+ function ageCal ( string $name, int $year){
+ $age = 2022 - $year;
+
+ echo "Hi {$name}, You are {$age} years old";
+
+ }
+
+?>
